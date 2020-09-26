b9d561fd9727d7387bbee99f4ca74358cc16f091
Christopher Williams <christopher.e.willi@wsu.edu>
2020-09-26 03:30:43 -0700

    Issue 1 branch created, SoulWarsGame core and 3 State skeleton created.

---
+++ soulwars/src/soulwars/SoulWarsGame.java
@@ -0,0 +1,52 @@
+package soulwars;
+
+import jig.Entity;
+import jig.ResourceManager;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class SoulWarsGame extends StateBasedGame {
+	
+	
+	//State Identitifiers
+	public static final int SPLASHSCREENSTATE = 0;
+	public static final int MAINMENUSTATE = 1;
+	public static final int PLAYINGSTATE = 2;
+	
+	//App properties
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 1024;
+	public static final int FPS = 60;
+	public static final double VERSION = .01;
+	
+	
+	public void initStatesList(GameContainer container) throws SlickException {
+		addState(new StartSplashState());
+		addState(new MainMenuState());
+		addState(new PlayingState());
+		
+	}
+	
+	public SoulWarsGame(String title) {
+		super(title);
+	}
+	
+	public static void main(String[] args) {
+		try {
+			AppGameContainer app = new AppGameContainer(new SoulWarsGame("SoulWars " + VERSION));
+			app.setDisplayMode(WIDTH, HEIGHT, false);
+			app.setTargetFrameRate(FPS);
+			app.setShowFPS(true);
+			app.start();
+		} catch(SlickException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
+

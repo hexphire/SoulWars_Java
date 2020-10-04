e40dff935a224d783a0e7ddaf97358143bb3f96e
Christopher Williams <christopher.e.willi@wsu.edu>
2020-10-04 05:54:46 -0700

    Added camera class and updated logic to handle it.

--- soulwars/src/soulwars/PlayingState.java
+++ soulwars/src/soulwars/PlayingState.java
@@ -11,14 +11,13 @@ import org.newdawn.slick.tiled.TiledMap;
 
 public class PlayingState extends BasicGameState {
 
-	private int pOneCameraX;
-	private int pOneCameraY;
+	SoulWarsCamera gameView;
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		// TODO Auto-generated method stub
-		pOneCameraX = 0;
-		pOneCameraY = 0;
+		SoulWarsGame swg = (SoulWarsGame)game;
+		gameView = new SoulWarsCamera(swg.gameMap);
 		
 	}
 	
@@ -36,13 +35,14 @@ public class PlayingState extends BasicGameState {
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
 		// TODO Auto-generated method stub
-		SoulWarsGame swg = (SoulWarsGame)game;
-		swg.gameMap.renderView(0, 0, pOneCameraX, pOneCameraY, g);
+		//render map
+		gameView.renderView(0, 0, g);
+		
 		//lets test getting map info from our tiledMap
 		//g.drawString("map size: "+swg.map.getWidth()+"x "+swg.map.getHeight()+"y", 30, 30);
 		//g.drawString("tile size: "+swg.map.getTileWidth()+"x "+swg.map.getTileHeight()+"y", 30, 60);
 		
-		//render map
+		
 		
 	}
 
@@ -57,33 +57,28 @@ public class PlayingState extends BasicGameState {
 		
 		//Camera Controls
 		if (input.isKeyDown(Input.KEY_W)){
-			if(pOneCameraY > 0)
-				pOneCameraY -= 1;
+			gameView.moveCameraY(-1);
 			
 		}		
 		if (input.isKeyDown(Input.KEY_S)){
-			if(pOneCameraY < swg.gameMap.getHeightInTiles() - 14)
-				pOneCameraY += 1;
+			gameView.moveCameraY(1);
 			
 		}
 
 		if (input.isKeyDown(Input.KEY_A)){
-			if(pOneCameraX > 0)
-				pOneCameraX -= 1;
+			gameView.moveCameraX(-1);
 			
-		}
+		}	
 		if (input.isKeyDown(Input.KEY_D)){
-			if(pOneCameraX < swg.gameMap.getWidthInTiles() - 16)
-				pOneCameraX += 1;
-			
+			gameView.moveCameraX(1);
 		}
 		
 		
 		//Mouse Controls
 		if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
-			float mouseX = (input.getMouseX() / 64) + pOneCameraX;
-			float mouseY = (input.getMouseY() / 64) + pOneCameraY;
-			System.out.println("x: " + mouseX + "y: " + mouseY);	
+			float mouseTileX = (input.getMouseX() / 64) + gameView.getCameraX();
+			float mouseTileY = (input.getMouseY() / 64) + gameView.getCameraY();
+			System.out.println("x: " + mouseTileX + "y: " + mouseTileY);	
 			
 		}
 	}
---
+++ soulwars/src/soulwars/SoulWarsCamera.java
@@ -0,0 +1,70 @@
+package soulwars;
+
+import org.newdawn.slick.Graphics;
+
+import jig.ResourceManager;
+
+public class SoulWarsCamera {
+	
+	private int xOffSet = 0;
+	private int yOffSet = 0;
+	private int tileWidth;
+	private int tileHeight;
+	private int mapHeight;
+	private int mapWidth;
+	
+	SoulWarsMap currentGame;
+	
+	
+	public SoulWarsCamera(SoulWarsMap map) {
+		currentGame = map;
+		tileWidth = currentGame.getTileWidth();
+		tileHeight = currentGame.getTileHeight();
+		mapHeight = currentGame.getHeightInTiles();
+		mapWidth = currentGame.getWidthInTiles();
+	}
+	
+	public void moveCameraX(int dx) {
+		if (dx > 0 && xOffSet < mapWidth - 16) {
+			xOffSet = xOffSet + 1;
+		}else if(dx < 0 && xOffSet > 0){
+			xOffSet = xOffSet - 1;
+		}
+		return;
+	}
+	
+	public void moveCameraY(int dy) {
+		if (dy > 0 && yOffSet < mapHeight - 14) {
+			yOffSet = yOffSet + 1;
+		}else if(dy < 0 && yOffSet > 0) {
+			yOffSet = yOffSet - 1;
+		}
+		
+		return;
+	}
+	
+	public int getCameraX() {
+		return xOffSet;
+	}
+	
+	public int getCameraY() {
+		return yOffSet;
+	}
+	
+	public void renderView(int x, int y, Graphics g) {
+		int[][] terrain = currentGame.getTerrain();
+		int tileId;
+		for (int xTile = 0; xTile < 16; xTile++) {
+			for (int yTile = 0; yTile < 14; yTile++) {
+				tileId = terrain[xTile + xOffSet][yTile + yOffSet];
+				if(tileId == 101) {
+					g.drawImage(ResourceManager.getImage(SoulWarsGame.TILE_RSC_101), xTile*tileWidth, yTile*tileHeight);
+				}else {
+					g.drawImage(ResourceManager.getImage(SoulWarsGame.TILE_RSC_59), xTile*tileWidth, yTile*tileHeight);
+				}							
+				
+			}
+		}
+		return;
+	}
+}

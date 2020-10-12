1873580d0a5a3d6f11622ebae1ae6023841de2c0
Christopher Williams <christopher.e.willi@wsu.edu>
2020-10-11 17:49:18 -0700

    Implemented Manhattan distance heuristic to use with A*

---
+++ soulwars/src/soulwars/ManhattanHeuristic.java
@@ -0,0 +1,19 @@
+package soulwars;
+
+import org.newdawn.slick.util.pathfinding.AStarHeuristic;
+import org.newdawn.slick.util.pathfinding.Mover;
+import org.newdawn.slick.util.pathfinding.TileBasedMap;
+
+public class ManhattanHeuristic implements AStarHeuristic{
+
+	@Override
+	public float getCost(TileBasedMap map, Mover mover, int x, int y, int tx, int ty) {
+			float dx = Math.abs(tx - x);
+			float dy = Math.abs(ty - y);
+			
+			float result = dx + dy;
+			
+		return result;
+	}
+
+}

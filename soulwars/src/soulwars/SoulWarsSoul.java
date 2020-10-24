1de09da53c42022d4bc9154c32f6546d5197f2e9
Christopher Williams <christopher.e.willi@wsu.edu>
2020-10-24 15:05:44 -0700

    Added initial code for soul entities

---
+++ soulwars/src/soulwars/SoulWarsSoul.java
@@ -0,0 +1,15 @@
+package soulwars;
+
+import jig.Entity;
+
+public class SoulWarsSoul extends Entity {
+	
+	int soulCount;
+	int lifeSpan;
+	
+	public SoulWarsSoul(float x, float y, int count, boolean limited) {
+		super(x,y);
+		//this.addImageWithBoundingBox();
+	}
+
+}

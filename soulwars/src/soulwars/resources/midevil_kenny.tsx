a6d54d028fe0b23de1aea4aaa5922e6556b619f0
Christopher Williams <christopher.e.willi@wsu.edu>
2020-10-25 23:35:17 -0700

    Map update

---
+++ soulwars/src/soulwars/resources/midevil_kenny.tsx
@@ -0,0 +1,382 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="midevil_kenny" tilewidth="64" tileheight="64" tilecount="126" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_01.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_02.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_03.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_04.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_05.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_06.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_07.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_08.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_09.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_18.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_19.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_20.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Environment/medievalEnvironment_21.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_01.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_02.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_03.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_04.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_05.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_06.png"/>
+ </tile>
+ <tile id="27">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_07.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_08.png"/>
+ </tile>
+ <tile id="29">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_09.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_10.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_11.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_12.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_13.png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_14.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_15.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_16.png"/>
+ </tile>
+ <tile id="37">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_17.png"/>
+ </tile>
+ <tile id="38">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_18.png"/>
+ </tile>
+ <tile id="39">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_19.png"/>
+ </tile>
+ <tile id="40">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_20.png"/>
+ </tile>
+ <tile id="41">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_21.png"/>
+ </tile>
+ <tile id="42">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_22.png"/>
+ </tile>
+ <tile id="43">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Structure/medievalStructure_23.png"/>
+ </tile>
+ <tile id="44">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_01.png"/>
+ </tile>
+ <tile id="45">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_02.png"/>
+ </tile>
+ <tile id="46">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_03.png"/>
+ </tile>
+ <tile id="47">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_04.png"/>
+ </tile>
+ <tile id="48">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_05.png"/>
+ </tile>
+ <tile id="49">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_06.png"/>
+ </tile>
+ <tile id="50">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_07.png"/>
+ </tile>
+ <tile id="51">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_08.png"/>
+ </tile>
+ <tile id="52">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_09.png"/>
+ </tile>
+ <tile id="53">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_10.png"/>
+ </tile>
+ <tile id="54">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_11.png"/>
+ </tile>
+ <tile id="55">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_12.png"/>
+ </tile>
+ <tile id="56">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_13.png"/>
+ </tile>
+ <tile id="57">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_14.png"/>
+ </tile>
+ <tile id="58">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_15.png"/>
+ </tile>
+ <tile id="59">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_16.png"/>
+ </tile>
+ <tile id="60">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_17.png"/>
+ </tile>
+ <tile id="61">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_18.png"/>
+ </tile>
+ <tile id="62">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_19.png"/>
+ </tile>
+ <tile id="63">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_20.png"/>
+ </tile>
+ <tile id="64">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_21.png"/>
+ </tile>
+ <tile id="65">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_22.png"/>
+ </tile>
+ <tile id="66">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_23.png"/>
+ </tile>
+ <tile id="67">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_24.png"/>
+ </tile>
+ <tile id="68">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_25.png"/>
+ </tile>
+ <tile id="69">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_26.png"/>
+ </tile>
+ <tile id="70">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_27.png"/>
+ </tile>
+ <tile id="71">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_28.png"/>
+ </tile>
+ <tile id="72">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_29.png"/>
+ </tile>
+ <tile id="73">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_30.png"/>
+ </tile>
+ <tile id="74">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_31.png"/>
+ </tile>
+ <tile id="75">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_32.png"/>
+ </tile>
+ <tile id="76">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_33.png"/>
+ </tile>
+ <tile id="77">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_34.png"/>
+ </tile>
+ <tile id="78">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_35.png"/>
+ </tile>
+ <tile id="79">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_36.png"/>
+ </tile>
+ <tile id="80">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_37.png"/>
+ </tile>
+ <tile id="81">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_38.png"/>
+ </tile>
+ <tile id="82">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_39.png"/>
+ </tile>
+ <tile id="83">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_40.png"/>
+ </tile>
+ <tile id="84">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_41.png"/>
+ </tile>
+ <tile id="85">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_42.png"/>
+ </tile>
+ <tile id="86">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_43.png"/>
+ </tile>
+ <tile id="87">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_44.png"/>
+ </tile>
+ <tile id="88">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_45.png"/>
+ </tile>
+ <tile id="89">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_46.png"/>
+ </tile>
+ <tile id="90">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_47.png"/>
+ </tile>
+ <tile id="91">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_48.png"/>
+ </tile>
+ <tile id="92">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_49.png"/>
+ </tile>
+ <tile id="93">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_50.png"/>
+ </tile>
+ <tile id="94">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_51.png"/>
+ </tile>
+ <tile id="95">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_52.png"/>
+ </tile>
+ <tile id="96">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_53.png"/>
+ </tile>
+ <tile id="97">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_54.png"/>
+ </tile>
+ <tile id="98">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_55.png"/>
+ </tile>
+ <tile id="99">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_56.png"/>
+ </tile>
+ <tile id="100">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_57.png"/>
+ </tile>
+ <tile id="101">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Tile/medievalTile_58.png"/>
+ </tile>
+ <tile id="102">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_01.png"/>
+ </tile>
+ <tile id="103">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_02.png"/>
+ </tile>
+ <tile id="104">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_03.png"/>
+ </tile>
+ <tile id="105">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_04.png"/>
+ </tile>
+ <tile id="106">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_05.png"/>
+ </tile>
+ <tile id="107">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_06.png"/>
+ </tile>
+ <tile id="108">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_07.png"/>
+ </tile>
+ <tile id="109">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_08.png"/>
+ </tile>
+ <tile id="110">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_09.png"/>
+ </tile>
+ <tile id="111">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_10.png"/>
+ </tile>
+ <tile id="112">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_11.png"/>
+ </tile>
+ <tile id="113">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_12.png"/>
+ </tile>
+ <tile id="114">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_13.png"/>
+ </tile>
+ <tile id="115">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_14.png"/>
+ </tile>
+ <tile id="116">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_15.png"/>
+ </tile>
+ <tile id="117">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_16.png"/>
+ </tile>
+ <tile id="118">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_17.png"/>
+ </tile>
+ <tile id="119">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_18.png"/>
+ </tile>
+ <tile id="120">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_19.png"/>
+ </tile>
+ <tile id="121">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_20.png"/>
+ </tile>
+ <tile id="122">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_21.png"/>
+ </tile>
+ <tile id="123">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_22.png"/>
+ </tile>
+ <tile id="124">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_23.png"/>
+ </tile>
+ <tile id="125">
+  <image width="64" height="64" source="kenney_medievalRTSpack/PNG/Default size/Unit/medievalUnit_24.png"/>
+ </tile>
+</tileset>
